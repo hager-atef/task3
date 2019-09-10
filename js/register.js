c5b51538ade94bb95206dd25da4049bf6289996c
unknown <smsmana923@gmail.com>
2019-09-10 15:54:54 +0200

    added search and log in and register

---
+++ js/register.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#logo").fadeIn(1000,function(){
+        $("#sign-up-heading").fadeIn(1000,function(){
+            $("#register-form").slideDown(2000,function(){
+                $("#new-doctor-heading").fadeIn(1000,function(){
+                    $("#doctor-form").slideDown(2000)
+                })
+            })
+        })
+    })
+    
+})
